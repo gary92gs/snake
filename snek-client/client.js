8d3b23d2c85463d97f03d202df5b9ce256a766cf
gary92gs <gary92.gs@gmail.com>
2024-01-17 17:20:12 -0700

    got user input/directional control working. function not separated yet though

--- snek-client/client.js
+++ snek-client/client.js
@@ -1,24 +1,19 @@
 const net = require("net");
 
 const connect = function () {
-  const conn = net.createConnection({
+  const connectSocket = net.createConnection({
     host: 'localhost',// IP address here,
     port: '50541'// PORT number here,
   });
-  conn.on('connect', stream => {
+  connectSocket.on('connect', stream => {
     console.log('connection established');
-    conn.write('Name: GDS');
-
-    
+    connectSocket.write('Name: GDS');
   });
-  // conn.on('listening',() => {
-  //   conn.write('Move:up');
-  // })
 
   // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on('data',(data) => console.log(data))
-  return conn;
+  connectSocket.setEncoding("utf8");
+  connectSocket.on('data',(data) => console.log(data))
+  return connectSocket;
 };
 
 module.exports = connect;
--- snek-client/play.js
+++ snek-client/play.js
@@ -1,7 +1,35 @@
 const net = require("net");
 const connect = require('./client');
+const stdin = process.stdin;
 
 
+const setupInput = function(socket) {
 
+  stdin.setRawMode(true); //makes it so that every keyboard entry is sent to the server immediately
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', (data) => {
+    if (data === '\u0003') {
+      process.exit();
+    }
+    if (data === 'w') {
+      socket.write('Move: up');
+    }
+    if (data === 'd') {
+      socket.write('Move: right');
+    }
+    if (data === 's') {
+      socket.write('Move: down');
+    }
+    if (data === 'a') {
+      socket.write('Move: left');
+    }
+  });
+return stdin;
+};
+
+const socket1 = connect();
+setupInput(socket1);
 console.log("Connecting ...");
-connect();
+
+
